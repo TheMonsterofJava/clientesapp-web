c5f431053a2ee5b33a4ad2a73593f39e5cda4beb
TheMonsterofjava <osvaldo_lovisolo4500@hotmail.com.ar>
2020-05-05 19:38:23 -0300

    Proyecto Creado y Configurado

---
+++ src/main/java/com/clientesapp/ClientesappApplication.java
@@ -0,0 +1,13 @@
+package com.clientesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesappApplication.class, args);
+	}
+
+}
